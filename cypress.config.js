2e6c2849a77e29c1558b5346f34e5462d7fd484a
Kinkini <kinkinigamage25@gmail.com>
2025-04-22 19:21:01 +1000

    added base url

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://restful-booker.herokuapp.com',
     excludeSpecPattern: ['**1-getting-started', '**2-advanced-examples'],
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
